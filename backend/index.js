8f8e10cb905561fe9d6949539705ef0c6c761e7a
RAM GURAV <ramgurav487@gmail.com>
2026-07-21 21:13:56 +0530

    Running First time Frontend and Backend initialy

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,13 @@
 import e from "express";
- 
- 
+
+const app = e();      
+
+
+app.get('/',(req,res)=>{
+    res.send("To do App")
+})
+
+const PORT = 5000;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
