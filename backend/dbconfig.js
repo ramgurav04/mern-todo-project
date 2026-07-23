f9ed19240ec3c283d82985d12edc5d992c73c11b
RAM GURAV <ramgurav487@gmail.com>
2026-07-23 20:47:42 +0530

    Dtabase Connected succes

--- backend/dbconfig.js
+++ backend/dbconfig.js
@@ -1,13 +1,16 @@
-const { MongoClient } = require("mongodb");
+import dns from "dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+import { MongoClient } from "mongodb";
 
-const url = "mongodb+srv://ramgurav487_db_user:root@cluster0.tmoubjb.mongodb.net/?appName=Cluster0";
-export const CollectionName = "Tasks";
-const dbname = "mern-todo";
+const url = "mongodb+srv://ramgurav487_db_user:root@mern-todo.hbhcrns.mongodb.net/?appName=mern-todo";
+
+export const CollectionName = "2";
+const dbname = "mern";
 
 export const client = new MongoClient(url);
 
 export const connection = async () => {
-    const connect = await client.connect();
-    const db = connect.db(dbname);
-    return await db;
+  const connect = await client.connect();
+  const db = connect.db(dbname);
+  return await db;
 };
--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,23 @@
 import e from "express";
-import { connection , collectionName } from "./dbconfig";
-const app = e();      
+import { connection, CollectionName } from "./dbconfig.js";
+const app = e();
 app.use(e.json());
 
-app.post("/add-task", (req, res) => {
-   const db = connection();
-   const collection = await db.collection("Tasks");
-   const result = await collection.insertOne(req.body);
-   res.send("Working")
+app.post("/add-task", async (req, res) => {
+  const db = await connection();
+  const collection = await db.collection(CollectionName);
+  const result = await collection.insertOne(req.body);
+  if(result){
+    res.send({ message: "Task added successfully", success: true });
+  } else {
+    res.send({ message: "Failed to add task", success: false });
+  }
 });
 
-app.get('/',(req,res)=>{
-    res.send("Home page");
-})
+app.get("/", (req, res) => {
+  res.send("Home page");
+});
 
-app.listen(3200);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
